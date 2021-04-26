e4d4d12c437032f9869eee21e0f93422591a5880
David Sadowsky <davidsadowsky0@gmail.com>
2021-04-26 00:25:35 -0400

    Deploy app to gh-pages

---
+++ src/App.js
@@ -0,0 +1,66 @@
+import './App.css';
+import React, { useEffect, useState } from 'react'
+import Box from '@material-ui/core/Box'
+import logo from './assets/Trend Invest.svg'
+import { makeStyles } from '@material-ui/core/styles'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import { AnimateOnChange } from 'react-animation'
+import Landing from './Screens/landing'
+import Results from './Components/Results'
+
+const useStyles = makeStyles((theme) => ({
+  logo: {
+    alignSelf: 'start'
+  },
+  body: {
+    display: 'flex',
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'column'
+  },
+  card: {
+    display: 'flex',
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'column'
+  },
+  title: {
+    color: 'white',
+    textAlign: 'center'
+  },
+  animateWrapper: {
+    display: 'flex',
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  }
+}))
+
+
+function App() {
+  useEffect(() => {
+    document.body.style.backgroundColor = '#1652F0'
+  }, [])
+
+  const [data, setData] = useState(null)
+  const [isLoading, setIsLoading] = useState(false)
+  const classes = useStyles()
+
+  return (
+    <div className={classes.body}>
+      <img src={logo} width='200' height='200' className={classes.logo}/>
+      <AnimateOnChange>
+        {data != null ? 
+          <Box className={classes.card}>
+            <Results onClear={setData} data={data} />
+          </Box> 
+          : 
+          <Landing setData={setData} isLoading={isLoading} setIsLoading={setIsLoading} />}
+      </AnimateOnChange>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/InputForm.js
@@ -0,0 +1,100 @@
+import React, { useState } from 'react'
+import { makeStyles, withStyles } from '@material-ui/core/styles'
+import Button from '@material-ui/core/Button'
+import Box from '@material-ui/core/Box'
+import Autocomplete from '@material-ui/lab/Autocomplete'
+import TextField from '@material-ui/core/TextField'
+import data from '../assets/crypto_data'
+
+const useStyles = makeStyles((theme) => ({
+    logo: {
+      marginLeft: theme.spacing(15)
+    },
+    body: {
+      padding: theme.spacing(10),
+      paddingLeft: theme.spacing(75),
+      paddingRight: theme.spacing(75)
+    },
+    form: {
+      backgroundColor: 'white',
+      padding: theme.spacing(5),
+      borderRadius: 5,
+      boxShadow: '0px 0px 10px 3px rgba(0, 0, 0, .3)',
+      display: 'flex',
+      flex: 1,
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'center'
+    }
+  }))
+
+  const BootstrapButton = withStyles({
+    root: {
+      color: 'white',
+      boxShadow: 'none',
+      textTransform: 'none',
+      fontSize: 14,
+      padding: '6px 200px',
+      border: '1px solid',
+      lineHeight: 1.5,
+      backgroundColor: '#1652F0',
+      borderColor: '#0063cc',
+      fontFamily: 'Red Hat Display',
+      '&:hover': {
+        backgroundColor: '#0069d9',
+        borderColor: '#0062cc',
+        boxShadow: 'none',
+      },
+      '&:active': {
+        boxShadow: 'none',
+        backgroundColor: '#0062cc',
+        borderColor: '#005cbf',
+      },
+      '&:focus': {
+        boxShadow: '0 0 0 0.2rem rgba(0,123,255,.5)',
+      },
+      '&:disabled': {
+        backgroundColor: '#bababa',
+        borderColor: '#bababa'
+      }
+    },
+  })(Button);
+
+const InputForm = ({setData, setIsLoading}) => {
+    const classes = useStyles()
+    const [selection, setSelection] = useState('')
+
+    const selectCoin = (params) => {
+        setSelection(params.inputProps.value)
+        return <TextField {...params} fontFamily="Red Hat Display" label="Coin" variant="outlined" />
+    }
+
+    const getPrediction = () => {
+      setIsLoading(true)
+      fetch('/predict?coinName=' + selection, {
+        method: "GET"
+      }).then(res => res.json().then(data => {
+        setData(data.data)
+        setIsLoading(false)
+      })
+    )}
+
+    return (
+        <form className={classes.form}>
+            <Autocomplete
+                id="combo-box-demo"
+                options={data}
+                getOptionLabel={(option) => option.title}
+                style={{ width: 300 }}
+                renderInput={(params) => selectCoin(params)}
+            />
+            <Box className={classes.submit} pt={2}>
+                <BootstrapButton disabled={selection === ''} onClick={() => getPrediction()}>
+                    Submit
+                </BootstrapButton>
+            </Box>
+        </form>
+    );
+}
+
+export default InputForm
